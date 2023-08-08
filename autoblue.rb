c0e5eed8748121ec94155424334a95f5b1f29c15
atayikilmaz <ata.yikilmaz@gmail.com>
2023-08-08 15:40:07 +0300

    autoblue

---
+++ autoblue.rb
@@ -0,0 +1,23 @@
+class Autoblue < Formula
+  desc "Automatically turning off Bluetooth when the Mac goes to sleep in macOS."
+  homepage "https://github.com/atayikilmaz/AutoBlue"
+  url "https://github.com/atayikilmaz/AutoBlue/releases/download/v0.1.0/AutoBlue.tar"
+  sha256 "82b0594afc16ae87c6b4ef45386293f2eab57264ef6bf1459884d7e475f5452e"
+  license "MIT"
+
+
+  def install
+    bin.install "autoblue"
+  end
+
+  test do
+    system "#{bin}/autoblue"
+  end
+
+
+
+  service do
+    run [opt_bin/"autoblue"]
+    keep_alive true
+  end
+end
